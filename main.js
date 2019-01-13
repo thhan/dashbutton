4275c1326cfeaa4f03431c4b4eb20d7eab05e229
thomas <th@sonntagnacht.de>
2019-01-13 17:02:54 +0100

    check device in network

---
+++ main.js
@@ -0,0 +1,41 @@
+const {exec} = require('child_process');
+
+const DashButtons = {
+  "B4:7C:9C:D7:BC:5D": {
+    Name: "Flur"
+  }
+};
+
+function scan(ops) {
+  var ans = [];
+  return new Promise(function(response, reject) {
+    exec('arp-scan -l -I ' + ops.dev, function(err, stdout, stderr) {
+      if (err) {
+        console.log("child processes failed with error code: " +
+                    err.code + stderr);
+      }
+      var net = stdout.split('\n');
+      for (var i = 2; i < net.length - 4; i++) {
+        var chunk = net[i].split('\t');
+        ans.push({'ip' : chunk[0], 'mac' : chunk[1].toUpperCase(), 'vendor' : chunk[2]});
+      }
+      response(ans);
+    });
+  });
+}
+
+
+function checkNetwork() {
+
+  scan({dev : 'en0'}).then((response) => {
+    for (let i = 0; i < response.length; i++) {
+      if (DashButtons[response[i].mac]) {
+        console.log(DashButtons[response[i].mac].Name)
+      }
+    }
+    checkNetwork()
+  });
+
+}
+
+checkNetwork()
